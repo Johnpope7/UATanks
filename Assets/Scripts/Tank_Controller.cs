d3ee1e5585c4c8a99159478c69fcc4274a72f0f3
Johnpope7 <Johnpope5387@gmail.com>
2021-06-15 22:39:30 -0700

    Updated Scripts
    Updated scripts for Tank_Controller, Shell_Bullet and Tank_Input to implement the firing of a bullet

--- Assets/Scripts/Tank_Controller.cs
+++ Assets/Scripts/Tank_Controller.cs
@@ -11,22 +11,31 @@ namespace TankSpace
     {
         #region Variables
         [Header("Movement Properties")]
-        public float tankSpeed = 15f;
-        public float tankRotationSpeed = 100f;
+        public float tankSpeed = 15f; //tanks movement speed
+        public float tankRotationSpeed = 100f; //tanks turning speed
 
         [Header("Turret Properties")]
-        public Transform turretTransform;
-        public float turretLagSpeed = 7f;
+        public Transform turretTransform; //transform for the tank turret
+        public float turretLagSpeed = 7f; //speed at which the turret lags behind the mouse position
 
         [Header("Reticle Properties")]
-        public Transform reticleTransform;
-
-
-
+        public Transform reticleTransform; //location of the reticle
 
         private Rigidbody trb; //stores the rigid bod required, presumably the tank
         private Tank_Inputs input; //stores the input for the tanks
-        private Vector3 finalTurretFacing;
+        private Vector3 finalTurretFacing; //the final direction the turret is facing
+
+        [Header("Tank Shell")]
+        public GameObject shell; //game object for the shell instantiation 
+        private Rigidbody srb; //stores the shell rigid body
+        private Transform firingZone; //the spot from which the bullet comes from
+        private static float shellTimer; //decides how long the bullet has till its destroyed
+
+        [Header("Tank Stats")]
+        private float shotForce = 200f;
+        [SerializeField]
+        private float tankDamage = 25f;
+
         #endregion
 
         #region Builtin Methods
@@ -40,18 +49,18 @@ namespace TankSpace
         // Update is called once per frame
         void FixedUpdate()
         {
-            if (trb && input) 
+            if (trb && input)
             {
                 HandleMovement();
                 HandleTurret();
                 HandleReticle();
-                
+
             }
         }
         #endregion
 
         #region Custom Methods
-        protected virtual void HandleMovement() 
+        protected virtual void HandleMovement()
         {
             //moves the tank forward and backwards
             Vector3 desiredPosition = transform.position + (transform.forward * input.ForwardInput * tankSpeed * Time.deltaTime);
@@ -62,22 +71,71 @@ namespace TankSpace
             trb.MoveRotation(desiredRotation);
         }
 
-        protected virtual void HandleTurret() 
+        protected virtual void HandleTurret() //handles the rotation of the turret head for the tank
         {
-            if (turretTransform) 
+            if (turretTransform)
             {
                 Vector3 turretFace = input.p_ReticlePosition - turretTransform.position;
                 turretFace.y = 0f;
                 finalTurretFacing = Vector3.Lerp(finalTurretFacing, turretFace, Time.deltaTime * turretLagSpeed);
                 turretTransform.rotation = Quaternion.LookRotation(finalTurretFacing);
-                
+
             }
         }
 
-        protected virtual void HandleReticle() 
+        protected virtual void HandleReticle()  //makes the reticle follow the mouse
         {
             reticleTransform.position = input.p_ReticlePosition;
         }
+
+
+        public void Shoot(float _shotforce)
+        {
+            //create the vector 3 variable that is equal to our firing zones forward vector multiplied by shot force
+            Vector3 shotDir = firingZone.forward * shotForce;
+            //spawn the bullet
+            GameObject shellInstance = Instantiate(shell, firingZone.position, firingZone.rotation);
+            //get the instigator
+            shellInstance.GetComponent<Shell_Bullet>().instigator = gameObject;
+            //get the shellDamage variable
+            shellInstance.GetComponent<Shell_Bullet>().SetShellDamage(tankDamage);
+            //get the shell rigid body to apply force
+            srb = shellInstance.GetComponent<Rigidbody>();
+            //apply the shotforce variable to the rigid body to make the bullet move
+            srb.AddForce(shotDir);
+            //destroy the bullet after a desired time
+            Destroy(shellInstance, shellTimer);
+        }
+
+        public void TakeDamage(float damage, GameObject instigator)
+        {
+            Debug.Log("Took Damage");
+        }
+
+        #region GettersAndSetters
+        public float GetTankDamage //the getter for the tank damage variable
+        {
+            get { return tankDamage; }
+        }
+        public float SetTankDamage(float dmg) //the setter of the tank damage variable
+        {
+            tankDamage = dmg;
+            return tankDamage;
+        }
+
+        public float GetShotForce
+        {
+            get { return shotForce; }
+        }
+
+        public float SetShotForce(float force)
+        {
+            shotForce = force;
+            return shotForce;
+        }
+
+        #endregion
+
         #endregion
 
     }
